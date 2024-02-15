68c5a68cd4dc71a03c9c84b3b7274f9101a4d917
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-02-15 15:55:01 +0300

    ssa

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,21 @@
 public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+
+
+
+    public void print() {
+        System.out.println("Teacher{" +
+                "name='" + name + '\n' +
+                ", mpno='" + mpno + '\n' +
+                ", branch='" + branch + '\n' +
+                '}');
+    }
 }
