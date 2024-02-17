485496040a97bec86a48e8a2d32283b91622fb78
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-02-17 08:54:05 +0300

    ssa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
+        Teacher t1 = new Teacher("Veli", "555", "TRH");
+        Teacher t2 = new Teacher("Einstein","333","FZK");
+        Teacher t3 = new Teacher("Ali","321","BIYO");
+
+
+        Course tarih = new Course("Tarih", "101", "TRH");
+        tarih.addTeacher(t1);
+
+
+        Course fizik = new Course("Fizik", "102","FZK");
+        fizik.addTeacher(t2);
+
+        Course biyoloji = new Course("Biyoloji", "321", "bıyo");
+        biyoloji.addTeacher(t3);
+
+
+
+        Student s1 = new Student("ekin","3","4",fizik,tarih,biyoloji);
+
+        s1.addExamNotes(30,20,30);
+
+        s1.isPass();
+
+
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,50 @@
 public class Student {
+    Course c1;
+    Course c2;
+    Course c3;
     String name;
+    String stuNo;
+    String classes;
+    double average;
+    boolean isPass;
+
+    Student(String name, String stuNo, String classes, Course c1, Course c2, Course c3) {
+        this.name = name;
+        this.stuNo = stuNo;
+        this.classes = classes;
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        this.average = 0;
+        this.isPass = false;
+    }
+
+    void addExamNotes(int note1, int note2, int note3) {
+        c1.note = note1;
+        c2.note = note2;
+        c3.note = note3;
+    }
+
+    void isPass(){
+        this.average = (this.c1.note + this.c2.note + this.c3.note)/3.0;
+        if(this.average > 55){
+            System.out.println("geçtiniz " + average);
+        }else{
+            System.out.println("kaldınız " + average );
+        }
+    }
+
+
+    public void printStu() {
+        System.out.println("Student{" +
+                "c1=" + c1.name + " :" + c1.note +
+                ", c2=" + c2.name +
+                ", c3=" + c3.name +
+                ", name='" + name + '\'' +
+                ", stuNo='" + stuNo + '\'' +
+                ", classes='" + classes + '\'' +
+                ", average=" + average +
+                ", isPass=" + isPass +
+                '}');
+    }
 }
